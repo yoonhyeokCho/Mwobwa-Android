f3af238445f9867ef50b5dd18c02119bb523bcbc
yoonhyeokCho <hyeok6640@gmail.com>
2023-08-02 23:41:36 +0900

    [feat] 처음 접속인지 아닌지 로직 구현

--- app/src/main/java/com/bongku/mwobwa/ui/select/SelectActivity.kt
+++ app/src/main/java/com/bongku/mwobwa/ui/select/SelectActivity.kt
@@ -1,14 +1,37 @@
 package com.bongku.mwobwa.ui.select
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Observer
 import com.bongku.mwobwa.databinding.ActivitySelectBinding
+import com.bongku.mwobwa.ui.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SelectActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySelectBinding
+    private val viewModel: SelectViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         binding = ActivitySelectBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel.checkFirstAccess()
+        viewModel.firstAccess.observe(this, Observer {
+            if(it){
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                viewModel.setFirstAccess()
+            }
+        })
     }
 }
