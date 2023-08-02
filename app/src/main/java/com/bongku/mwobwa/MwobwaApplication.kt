d5d85281d86e75ece965f79734611983dcf055f2
yoonhyeokCho <hyeok6640@gmail.com>
2023-08-02 23:40:24 +0900

    [feat] MwobwaApplication.kt에 dataStore 선언

--- app/src/main/java/com/bongku/mwobwa/MwobwaApplication.kt
+++ app/src/main/java/com/bongku/mwobwa/MwobwaApplication.kt
@@ -1,8 +1,21 @@
 package com.bongku.mwobwa
 
 import android.app.Application
+import com.bongku.mwobwa.data.datastore.MwobwaDataStore
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class MwobwaApplication : Application() {
+    @Inject
+    lateinit var mwobwaDataStore: MwobwaDataStore
+
+    companion object{
+        lateinit var mwobwaApplication: MwobwaApplication
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mwobwaApplication = this
+    }
 }
