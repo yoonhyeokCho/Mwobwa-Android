521a77faf92150d0e028806a1388baee37193ada
yoonhyeokCho <hyeok6640@gmail.com>
2023-08-04 00:03:29 +0900

    [feat] Contents response 데이터 클래스 정의

---
+++ app/src/main/java/com/bongku/mwobwa/data/entity/ContentsEntity.kt
@@ -0,0 +1,22 @@
+package com.bongku.mwobwa.data.entity
+import com.google.gson.annotations.SerializedName
+
+data class ContentsEntity (
+    val results: List<Result>
+)
+
+data class Result(
+    val adult: Boolean,
+    @SerializedName("genre_ids")
+    val genreIDS: List<Long>,
+    @SerializedName("original_title")
+    val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    val title: String,
+    val video: Boolean,
+    @SerializedName("vote_average")
+    val voteAverage: Double,
+    @SerializedName("vote_count")
+    val voteCount: Long
+)
