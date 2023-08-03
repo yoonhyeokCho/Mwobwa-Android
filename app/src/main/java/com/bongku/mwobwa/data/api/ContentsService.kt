9102338772aec51f1bb0e3a1c53b43a92cce9569
yoonhyeokCho <hyeok6640@gmail.com>
2023-08-04 00:02:21 +0900

    [feat] TMDB 관련 서비스 정의

---
+++ app/src/main/java/com/bongku/mwobwa/data/api/ContentsService.kt
@@ -0,0 +1,20 @@
+package com.bongku.mwobwa.data.api
+
+import com.bongku.mwobwa.data.entity.ContentsEntity
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ContentsService {
+    @GET("discover/{mediaType}")
+    suspend fun getContents(
+        @Path("mediaType") mediaType: String,
+        @Query("api_key") api_key: String,
+        @Query("include_adult") includeAdult: Boolean,
+        @Query("language") language: String = "ko-KR",
+        @Query("page") page: Int,
+        @Query("sort_by") sortBy: String = "popularity.desc",
+        @Query("with_companies") withCompanies: String,
+    ): ContentsEntity
+}
