b74aa4c779fa4f146a01ceeaaf806020eb283278
yoonhyeokCho <hyeok6640@gmail.com>
2023-08-03 18:26:11 +0900

    [feat] NavController setting

--- app/src/main/java/com/bongku/mwobwa/ui/MainActivity.kt
+++ app/src/main/java/com/bongku/mwobwa/ui/MainActivity.kt
@@ -2,19 +2,31 @@ package com.bongku.mwobwa.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.bongku.mwobwa.R
 import com.bongku.mwobwa.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    
     private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initBottomNav()
     }
 
+    private fun initBottomNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+    }
 }
