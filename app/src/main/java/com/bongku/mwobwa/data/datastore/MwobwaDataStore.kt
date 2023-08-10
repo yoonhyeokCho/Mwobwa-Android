db1eb96f1c155ddc4bbc10ab7c86d53eacdf6ab3
yoonhyeokCho <hyeok6640@gmail.com>
2023-08-10 17:27:14 +0900

    [feat] FIRSTACCESS 키 값 제거 및 관련 로직 삭제, Ott Company 관련 키 추가 및 관련 함수 정의

--- app/src/main/java/com/bongku/mwobwa/data/datastore/MwobwaDataStore.kt
+++ app/src/main/java/com/bongku/mwobwa/data/datastore/MwobwaDataStore.kt
@@ -1,32 +1,83 @@
 package com.bongku.mwobwa.data.datastore
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MwobwaDataStore @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
     private val Context.dataStore by preferencesDataStore(name = "dataStore")
-    private val FIRSTACCESS = booleanPreferencesKey("FIRSTACCESS")
 
-    suspend fun getFirstAccess(): Boolean {
-        val accessValue: Flow<Boolean> = context.dataStore.data
-            .map { preferences ->
-                preferences[FIRSTACCESS] ?: false
-            }
-        return accessValue.first()
-    }
+    private val NETFLIX = booleanPreferencesKey("NETFLIX")
+    private val DISNEY = booleanPreferencesKey("DISNEY")
+    private val COUPANG = booleanPreferencesKey("COUPANG")
 
-    suspend fun setFirstAccess() {
+    suspend fun initializeOttCompanies() {
         context.dataStore.edit { preferences ->
-            preferences[FIRSTACCESS] = true
+            preferences[NETFLIX] = false
+            preferences[DISNEY] = false
+            preferences[COUPANG] = false
         }
     }
+
+    suspend fun setOttCompany(contents: List<String>) {
+        initializeOttCompanies()
+
+        if (!contents.isEmpty()) {
+            for (it in contents) {
+                if (it == "NETFLIX") {
+                    context.dataStore.edit { preferences ->
+                        preferences[NETFLIX] = true
+                    }
+                } else if (it == "DISNEY") {
+                    context.dataStore.edit { preferences ->
+                        preferences[DISNEY] = true
+                    }
+                } else {
+                    context.dataStore.edit { preferences ->
+                        preferences[COUPANG] = true
+                    }
+                }
+            }
+        }
+    }
+
+    suspend fun getOttCompany(): List<String> {
+        val contentsList = mutableListOf<String>()
+
+        val netflixValue: Flow<Boolean> = context.dataStore.data
+            .map { preferences ->
+                preferences[NETFLIX] ?: false
+            }
+        val disneyValue: Flow<Boolean> = context.dataStore.data
+            .map { preferences ->
+                preferences[DISNEY] ?: false
+            }
+        val coupangValue: Flow<Boolean> = context.dataStore.data
+            .map { preferences ->
+                preferences[COUPANG] ?: false
+            }
+
+        if (netflixValue.first()) {
+            contentsList.add("netflix")
+        }
+        if (disneyValue.first()) {
+            contentsList.add("disney")
+        }
+        if (coupangValue.first()) {
+            contentsList.add("coupang")
+        }
+        return contentsList
+    }
+
 }
