7823f657e40198377c503d6265b0e33581725168
yoonhyeokCho <hyeok6640@gmail.com>
2023-08-10 19:36:05 +0900

    [feat] getContents 함수에 compnay 정보 셋팅하는 로직 추가

--- app/src/main/java/com/bongku/mwobwa/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bongku/mwobwa/ui/home/HomeViewModel.kt
@@ -1,8 +1,11 @@
 package com.bongku.mwobwa.ui.home
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bongku.mwobwa.MwobwaApplication
+import com.bongku.mwobwa.data.datastore.MwobwaDataStore
 import com.bongku.mwobwa.data.entity.ContentsEntity
 import com.bongku.mwobwa.data.repository.ContentsRepositoryImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,6 +20,12 @@ class HomeViewModel @Inject constructor(
     private val _contentsResponse = MutableLiveData<ContentsEntity>()
     val contentsResponse get() = _contentsResponse
 
+    private val _ottCompany = MutableLiveData<List<String>>()
+    val ottCompany get() = _ottCompany
+
+    private val mwobwaDataStore: MwobwaDataStore =
+        MwobwaApplication.mwobwaApplication.mwobwaDataStore
+
     fun getContents(
         mediaType: String,
         includeAdult: Boolean,
@@ -30,7 +39,15 @@ class HomeViewModel @Inject constructor(
                 page,
                 withCompanies
             )
+            response.results.forEach {
+                it.company = withCompanies
+            }
             _contentsResponse.value = response
         }
     }
+
+    fun getOttCompany() = viewModelScope.launch {
+        _ottCompany.value = mwobwaDataStore.getOttCompany()
+    }
+
 }
