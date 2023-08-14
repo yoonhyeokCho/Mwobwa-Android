1f117ef579b19339218c81d7c18d30ae5914aba4
yoonhyeokCho <hyeok6640@gmail.com>
2023-08-14 19:57:13 +0900

    [feat] 이미지 url에서 가져와 띄우기 (Glide)

--- app/src/main/java/com/bongku/mwobwa/ui/home/HomeRVAdapter.kt
+++ app/src/main/java/com/bongku/mwobwa/ui/home/HomeRVAdapter.kt
@@ -1,6 +1,7 @@
 package com.bongku.mwobwa.ui.home
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,8 @@ import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import com.bongku.mwobwa.R
 import com.bongku.mwobwa.data.entity.ContentsResult
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 
 class HomeRVAdapter(
     val context: Context,
@@ -22,15 +25,12 @@ class HomeRVAdapter(
         val plus_img = view.findViewById<ImageView>(R.id.item_plus)
         val ggim = view.findViewById<TextView>(R.id.item_ggimText)
         val story = view.findViewById<TextView>(R.id.item_story)
-        val company = view.findViewById<TextView>(R.id.item_company_text)
+        val ottCompany = view.findViewById<TextView>(R.id.item_company_text)
         val title = view.findViewById<TextView>(R.id.item_title)
         val vote_score = view.findViewById<TextView>(R.id.item_vote_score)
         val vote_count = view.findViewById<TextView>(R.id.item_vote_count)
-        val spinner = view.findViewById<Spinner>(R.id.item_genre_spinner)
-
-        init {
-            initSpinner(spinner)
-        }
+        val genre = view.findViewById<TextView>(R.id.item_genre_text)
+        val poster = view.findViewById<ImageView>(R.id.item_image_view)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -45,21 +45,17 @@ class HomeRVAdapter(
             vote_count.text = "참가자 : " + dataSet[position].voteCount.toString()
             story.text = dataSet[position].overview
             title.text = dataSet[position].title
+            ottCompany.text = dataSet[position].company
+            genre.text = "장르 : " + dataSet[position].genreIDS.toString()
+
+            Glide.with(itemView)
+                .load("https://image.tmdb.org/t/p/w342${dataSet[position].poster_path}")
+                .transform(CenterCrop())
+                .into(poster)
         }
     }
 
     override fun getItemCount(): Int {
         return dataSet.size
-    }
-
-    private fun initSpinner(spinner: Spinner) {
-        ArrayAdapter.createFromResource(
-            context,
-            R.array.spinner_genre_array,
-            android.R.layout.simple_spinner_item
-        ).also {
-            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = it
-        }
     }
 }
