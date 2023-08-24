533d35ff51448b2249a2f01e91b63020fa0af09c
yoonhyeokCho <hyeok6640@gmail.com>
2023-08-24 17:20:06 +0900

    [refactor] 코드 가독성을 위해 View Binding 사용하여 코드 수정

--- app/src/main/java/com/bongku/mwobwa/ui/home/HomeRVAdapter.kt
+++ app/src/main/java/com/bongku/mwobwa/ui/home/HomeRVAdapter.kt
@@ -1,18 +1,16 @@
 package com.bongku.mwobwa.ui.home
 
 import android.content.Context
-import android.media.Image
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.ImageView
-import android.widget.Spinner
 import android.widget.TextView
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import com.bongku.mwobwa.R
 import com.bongku.mwobwa.data.entity.ContentsResult
+import com.bongku.mwobwa.databinding.ItemHomeBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 
@@ -21,38 +19,87 @@ class HomeRVAdapter(
     val dataSet: List<ContentsResult>
 ) : RecyclerView.Adapter<HomeRVAdapter.ViewHolder>() {
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val plus_img = view.findViewById<ImageView>(R.id.item_plus)
-        val ggim = view.findViewById<TextView>(R.id.item_ggimText)
-        val story = view.findViewById<TextView>(R.id.item_story)
-        val ottCompany = view.findViewById<TextView>(R.id.item_company_text)
-        val title = view.findViewById<TextView>(R.id.item_title)
-        val vote_score = view.findViewById<TextView>(R.id.item_vote_score)
-        val vote_count = view.findViewById<TextView>(R.id.item_vote_count)
-        val genre = view.findViewById<TextView>(R.id.item_genre_text)
-        val poster = view.findViewById<ImageView>(R.id.item_image_view)
+    interface ItemClick {
+        fun onGgimClick(contentsEntity: ContentsResult)
+    }
+
+    var itemClick: ItemClick? = null
+
+    inner class ViewHolder(val binding: ItemHomeBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.run {
+                itemPlus.setOnClickListener {
+                    itemClick?.onGgimClick(dataSet[adapterPosition])
+                }
+                itemGgimText.setOnClickListener {
+                    itemClick?.onGgimClick(dataSet[adapterPosition])
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_home, parent, false)
-        return ViewHolder(view)
+        val binding = ItemHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.run {
-            title.text = dataSet[position].title
-            vote_score.text = "평점 : " + dataSet[position].voteAverage.toString()
-            vote_count.text = "참가자 : " + dataSet[position].voteCount.toString()
-            story.text = dataSet[position].overview
-            title.text = dataSet[position].title
-            ottCompany.text = dataSet[position].company
-            genre.text = "장르 : " + dataSet[position].genreIDS.toString()
+        holder.binding.run {
+            itemTitle.text = dataSet[position].title
+            itemVoteScore.text = "평점 : " + dataSet[position].voteAverage.toString()
+            itemVoteCount.text = "참가자 : " + dataSet[position].voteCount.toString()
+            itemStory.text = dataSet[position].overview
+            itemTitle.text = dataSet[position].title
+            itemCompanyText.text = dataSet[position].company
 
-            Glide.with(itemView)
+            val contentsGenre = genreIdToStr(dataSet[position].genreIDS)
+            itemGenreText.text = "장르 : " + contentsGenre
+
+            Glide.with(itemImageView)
                 .load("https://image.tmdb.org/t/p/w342${dataSet[position].poster_path}")
                 .transform(CenterCrop())
-                .into(poster)
+                .into(itemImageView)
         }
+    }
+
+    private fun genreIdToStr(ids: List<Long>): String {
+        Log.d("genre", "genreIdToStr: ${ids}")
+        val genreMapping = mapOf(
+            28L to "Action",
+            12L to "Adventure",
+            16L to "Animation",
+            35L to "Comedy",
+            80L to "Crime",
+            99L to "Documentary",
+            18L to "Drama",
+            10751L to "Family",
+            14L to "Fantasy",
+            36L to "History",
+            27L to "Horror",
+            10402L to "Music",
+            9648L to "Mystery",
+            10749L to "Romance",
+            878L to "Science Fiction",
+            10770L to "TV Movie",
+            53L to "Thriller",
+            10752L to "War",
+            37L to "Western",
+            10759L to "Action & Adventure",
+            10762L to "Kids",
+            10763L to "News",
+            10764L to "Reality",
+            10765L to "Sci-Fi & Fantasy",
+            10766L to "Soap",
+            10767L to "Talk",
+            10768L to "War & Politics"
+        )
+
+        val mappedGenre = ids.mapNotNull {
+            genreMapping[it]
+        }
+
+        return mappedGenre.joinToString(", ")
     }
 
     override fun getItemCount(): Int {
