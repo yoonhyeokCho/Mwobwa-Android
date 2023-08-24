5b8107f0be938455060b3bb14821bc0062fbeb49
yoonhyeokCho <hyeok6640@gmail.com>
2023-08-24 17:23:05 +0900

    [refactor] 코드 가독성을 위해 View Binding 사용하여 코드 수정

--- app/src/main/java/com/bongku/mwobwa/ui/search/SearchRVAdapter.kt
+++ app/src/main/java/com/bongku/mwobwa/ui/search/SearchRVAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bongku.mwobwa.R
 import com.bongku.mwobwa.data.entity.ContentsResult
+import com.bongku.mwobwa.databinding.ItemSearchBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 
@@ -17,27 +18,25 @@ class SearchRVAdapter(
     val dataSet: List<ContentsResult>
 ) : RecyclerView.Adapter<SearchRVAdapter.ViewHolder>() {
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val title = view.findViewById<TextView>(R.id.item_search_title)
-        val vote_score = view.findViewById<TextView>(R.id.item_search_score)
-        val poster = view.findViewById<ImageView>(R.id.item_search_image_view)
+    inner class ViewHolder(val binding: ItemSearchBinding) : RecyclerView.ViewHolder(binding.root) {
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_search, parent, false)
-        return ViewHolder(view)
+        val binding = ItemSearchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.run {
-            title.text = dataSet[position].title
-            vote_score.text = dataSet[position].voteAverage.toString() + "점"
+        holder.binding.run {
+            itemSearchTitle.text = dataSet[position].title
+            itemSearchScore.text = dataSet[position].voteAverage.toString() + "점"
 
             if (dataSet[position].poster_path != null) {
-                Glide.with(itemView)
+                Glide.with(itemSearchImageView)
                     .load("https://image.tmdb.org/t/p/w342${dataSet[position].poster_path}")
                     .transform(CenterCrop())
-                    .into(poster)
+                    .into(itemSearchImageView)
             }
         }
     }
